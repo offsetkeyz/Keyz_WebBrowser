b91b3dd84fd81331e7b211b8fced96ff8af70df0
Colin <colin.mcallister89@yahoo.com>
2020-09-14 22:06:43 -0700

    Finished GetAllHistoryItems() in the History Manager

--- WebBrowser/WebBrowser.Logic/HistoryManager.cs
+++ WebBrowser/WebBrowser.Logic/HistoryManager.cs
@@ -10,6 +10,9 @@ namespace WebBrowser.Logic
 {
     public class HistoryManager
     {
+        /**
+         * Adds a HistoryItem to the database
+         */ 
         public static void AddHistoryItem(HistoryItem item)
         {
             DateTime currentDateTime = DateTime.Now;
@@ -17,12 +20,23 @@ namespace WebBrowser.Logic
             adapter.Insert(item.URL, item.Title, item.Date); 
         }
 
+        /**
+         * Gets all History Items in the database.
+         * @return a list of all history items
+         */
         public static List<HistoryItem> GetAllHistoryItems()
         {
             var adapter = new HistoryTableAdapter();
             var results = new List<HistoryItem>();
             var rows = adapter.GetData();
 
+            foreach(var row in rows)
+            {
+                var item = new HistoryItem(row.URL, row.Title, row.Date);
+
+                results.Add(item); 
+            }
+            // TODO paused video at 17:30
             return results; 
         }
     }
