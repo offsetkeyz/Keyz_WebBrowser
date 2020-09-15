987654d44a0b3a716eada80b8f92f6bebed780b6
Colin <colin.mcallister89@yahoo.com>
2020-09-14 21:39:56 -0700

    Added constructor to HistoryItem class

---
+++ WebBrowser/WebBrowser.Logic/HistoryManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBrowser.Logic
+{
+    public class HistoryManager
+    {
+        public static void AddHistoryItem(string URL, string Title)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            HistoryItem newItem = new HistoryItem(URL, Title, currentDateTime); 
+        }
+    }
+}
